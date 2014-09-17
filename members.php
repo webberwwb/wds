3c7129554cc9f588b8f7e5c7a36ad7fd498eb97c
webberwwb <webberwwb@gmail.com>
2014-09-17 19:40:03 -0300

    added weibo's name

--- members.php
+++ members.php
@@ -7,7 +7,9 @@ $members_array = array(
 	array("name" => "Kyle Asaff", "position" => "Member", "img" => "kyle.jpg"),
 	array("name" => "Matthew Coelho", "position" => "Member", "img" => "coelho.jpg"),
 	array("name" => "Terry Lin", "position" => "Member", "img" => "terry.jpg"),
-	array("name" => "Brenden Krochko", "position" => "Member", "img" => "brenden.jpg")
+	array("name" => "Brenden Krochko", "position" => "Member", "img" => "brenden.jpg"),
+	array("name" => "Weibo Wang", "position" => "Member", "img" => "")
+);
 );
 
 ?>
